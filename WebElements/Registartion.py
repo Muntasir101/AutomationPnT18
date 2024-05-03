e4d47b99b98a7a3d2f834e1ea5e31e373aa22fd5
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-05-03 15:41:24 +0600

    Registration verify

--- WebElements/Registartion.py
+++ WebElements/Registartion.py
@@ -12,18 +12,18 @@ driver.get("https://tutorialsninja.com/demo/index.php?route=account/register")
 time.sleep(2)
 
 firstName = driver.find_element(By.CSS_SELECTOR, "input#input-firstname")
-firstName.send_keys("Mr.Test")
+firstName.send_keys(random_strings.random_lowercase(5))
 
 lastName = driver.find_element(By.CSS_SELECTOR, "input#input-lastname")
 lastName.send_keys("Selenium")
 
 email = driver.find_element(By.CSS_SELECTOR, "#input-email")
-email.send_keys(random_strings.random_lowercase(10)+"@gmail.com")
+email.send_keys(random_strings.random_lowercase(10) + "@gmail.com")
 
 telephone = driver.find_element(By.CSS_SELECTOR, "#input-telephone")
 telephone.send_keys("123456")
 
-password = driver.find_element(By.CSS_SELECTOR,"#input-password")
+password = driver.find_element(By.CSS_SELECTOR, "#input-password")
 password.send_keys("1234567")
 
 confirmPassword = driver.find_element(By.CSS_SELECTOR, "#input-confirm")
@@ -32,5 +32,16 @@ confirmPassword.send_keys("1234567")
 privacy = driver.find_element(By.CSS_SELECTOR, "input[name='agree']")
 privacy.click()
 
-continueButton = driver.find_element(By.CSS_SELECTOR,".btn-primary")
+continueButton = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
 continueButton.click()
+
+# Verify account create success message
+expected_Success_Message = "Your Account Has Been Created!"
+
+success_message = driver.find_element(By.CSS_SELECTOR, "#content h1")
+actual_success_message_text = success_message.text
+
+if expected_Success_Message == actual_success_message_text:
+    print("Test Pass.Account Created Successfully")
+else:
+    print("Test Fail.Account Creation Failed")
